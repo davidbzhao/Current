a57ec7c6d38d731c53fb7a631587042f3f7360d6
David Zhao <dzhao@idtus.com>
2017-07-26 12:52:19 -0400

    Set up foundation

---
+++ js/app.js
@@ -0,0 +1,9 @@
+require.config({
+    baseUrl: 'js',
+    paths: {
+        jquery: 'libs/jquery-1.11.3.min',
+        underscore: 'libs/underscore.min',
+        bootstrap: 'libs/bootstrap.min',
+        text: 'libs/text'
+    }
+})
