e5d093a3b1d8cd6240b759393b16711d184349b8
David Zhao <dzhao@idtus.com>
2017-07-27 11:53:23 -0400

    Implement item adder

--- js/item.js
+++ js/item.js
@@ -2,23 +2,32 @@ define([
     'jquery',
 	'underscore',
 	'firebase',
-	'text!../../partials/item.html'
-], function($, _, firebase, ItemTemplate){
+	'text!../../partials/item.html',
+	'text!../../partials/itemAdder.html'
+], function($, _, firebase, ItemTemplate, ItemAdderTemplate){
 	
-	var config = {
-		apiKey: "AIzaSyBZjK-fcZ0Q8SlgWLbnakzymQRt01cjKPE",
-		authDomain: "current-ad2ee.firebaseapp.com",
-		databaseURL: "https://current-ad2ee.firebaseio.com",
-		projectId: "current-ad2ee",
-		storageBucket: "",
-		messagingSenderId: "1005389711220"
-	};
-	firebase.initializeApp(config);
-
 	$(document).ready(function(){
+		initializeFirebase();
 		initializeSignInButtonListener();
 		loadItems();
-	})
+	});
+
+	function initializeFirebase(){
+		var config = {
+			apiKey: "AIzaSyBZjK-fcZ0Q8SlgWLbnakzymQRt01cjKPE",
+			authDomain: "current-ad2ee.firebaseapp.com",
+			databaseURL: "https://current-ad2ee.firebaseio.com",
+			projectId: "current-ad2ee",
+			storageBucket: "",
+			messagingSenderId: "1005389711220"
+		};
+		firebase.initializeApp(config);
+		firebase.auth().onAuthStateChanged(function(user){
+			if(user){
+				loadItemAdder();
+			}
+		});
+	}
 
 	function initializeSignInButtonListener(){
 		$('#signin').click(function(){
@@ -31,20 +40,6 @@ define([
 		});
 	}
 
-	function addTestItem(){
-		var newItemKey = firebase.database().ref('items/').push().key;
-		var newItemObj = {}
-		newItemObj = {
-			body: "Hello mundo!",
-			timestamp: (new Date).getTime()
-		}
-		newItemObj['tags'] = {
-			'test': true,
-			'fun': true
-		}
-		firebase.database().ref('items/').push(newItemObj)
-	}
-
 	function loadItems(){
 		firebase.database().ref('/items').once('value').then(function(snapshot){
 			var jsonItems = snapshot.val();
@@ -52,8 +47,8 @@ define([
 			var newItemTemplate = _.template(ItemTemplate);
 			for(var cnt = 0; cnt < itemKeys.length; cnt++){
 				var time = new Date(jsonItems[itemKeys[cnt]].timestamp);
-				var month = time.getMonth();
-				var day = time.getDate() + 1;
+				var month = time.getMonth() + 1;
+				var day = time.getDate();
 				var content = jsonItems[itemKeys[cnt]].body;
 				var tags = []
 				if(jsonItems[itemKeys[cnt]].tags !== undefined){
@@ -68,5 +63,102 @@ define([
 				$('#items').append(newItemHtml);				
 			}
 		});
+	}
+
+	function loadItemAdder(){
+		if(firebase.auth().currentUser != null){
+			if(firebase.auth().currentUser.email === 'davidzhao058@gmail.com'){
+				var newItemAdderTemplate = _.template(ItemAdderTemplate);
+				var newDate = new Date();
+				var currentYear = newDate.getFullYear();
+				var currentMonth = newDate.getMonth() + 1;
+				var currentDay = newDate.getDate();
+				var newItemAdderHtml = newItemAdderTemplate({
+					currentYear: currentYear,
+					currentMonth: currentMonth,
+					currentDay: currentDay
+				});
+				$('#main').prepend(newItemAdderHtml);
+				$('#itemAdder-content').focus();
+				initializeItemAdderListener();
+			}
+		}
+	}
+
+	function isNumKey(evt){
+		var keycode = (evt.which) ? evt.which : evt.keyCode;
+		if(keycode > 31 && !(keycode >= 48 && keycode <= 57)){
+			return false;
+		}
+		return true;
+	}
+
+	function isCorrectLength(evt){
+		var target = $(evt.target);
+		if(target.prop('id') === 'itemAdder-year'){
+			return (target.val().length < 4) && (target.val().length >= 0)
+		} else if(target.prop('id') === 'itemAdder-month'){
+			return (target.val().length < 2) && (target.val().length >= 0)
+		} else if(target.prop('id') === 'itemAdder-day'){
+			return (target.val().length < 2) && (target.val().length >= 0)
+		}
+	}
+
+	function verify(evt){
+		return isNumKey(evt) && isCorrectLength(evt);
+	}
+
+	function initializeItemAdderListener(){
+		$('#main').on('keypress','.itemAdder-date', verify);
+		$('#main').on('change','input', function(evt){
+			$(evt.target).css('border','');
+		});
+		$('#itemAdder-form').submit(function(e){
+			e.preventDefault();
+			var submitForm = true;
+			
+			var submittedYear = parseInt($('#itemAdder-year').val());
+			var submittedMonth = parseInt($('#itemAdder-month').val());
+			var submittedDay = parseInt($('#itemAdder-day').val());
+			var submittedDate = new Date(submittedYear + '/' + submittedMonth + '/' + submittedDay);
+			if(isNaN(submittedDate.getTime())){
+				submitForm = false;
+				$('.itemAdder-date').css('border','solid 2px #f33');
+			}
+			var submittedContent = $('#itemAdder-content').val().trim();
+			if(submittedContent === ''){
+				submitForm = false;
+			}
+			var submittedTags = $('#itemAdder-tags').val().split(',');
+			if(submitForm){
+				var time = submittedDate.getTime();
+				var content = submittedContent;
+				var tags = {}
+				for(var i in submittedTags){
+					if(submittedTags[i].trim() !== ''){
+						tags[submittedTags[i].trim()] = true;
+					}
+				}
+
+				addItem(time, content, tags);
+				var newItemTemplate = _.template(ItemTemplate);
+				var newItemHtml = newItemTemplate({
+					month: submittedMonth,
+					day: submittedDay,
+					content: content,
+					tags: _.keys(tags)
+				});
+				$('#items').prepend(newItemHtml);
+			}
+		});
+	}
+
+	function addItem(time, content, tags){
+		var newItemObj = {
+			body: content,
+			timestamp: time,
+			tags: tags
+		}
+		firebase.database().ref('items/').push(newItemObj);
 	}
 });
